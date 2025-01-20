1a03cfd5ff98ecfe294b846205761f25c2012e34
AukTo1380 <auktoafg@gmail.com>
2025-01-20 23:32:06 +0430

    testing api

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,11 @@
+import Hello from "../commponents/hello";
+
+export const metadata = {
+  title: "Main",
+  description: "Home page",
+  keywords: "Home ",
+  // image:{`../public/images/nextjs.png`},
+};
 export default function Home() {
   console.log("beckend messages");
   return (
@@ -5,6 +13,7 @@ export default function Home() {
       <h1 className="text-center text-3xl font-bold">
         Wellcom the my first next porject
       </h1>
+      <Hello/>
     </>
   );
 }
