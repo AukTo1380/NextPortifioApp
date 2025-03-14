49e6328248db5bcb28e005d5e6c6a2f6f76042c2
AukTo1380 <auktoafg@gmail.com>
2025-03-14 20:26:43 +0430

    h

--- my-app/app/components/gfgf.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function gfgf() {
-  return (
-    <div>gfgf</div>
-  )
-}
-
-export default gfgf
--- my-app/app/components/gg.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function gg() {
-  return (
-    <div>gg</div>
-  )
-}
-
-export default gg
